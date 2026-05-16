3bc1b7a3584761a063b0b8cb80cd65c5970107cf
MABD <mabd.universe@gmail.com>
2026-05-16 15:35:36 +0300

    Initial Commit

---
+++ paws/pawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  pawsApp.swift
+//  paws
+//
+//  Created by Mahmoud Abdallah on 16/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct pawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
